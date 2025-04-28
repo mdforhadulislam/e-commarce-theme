05d9062bcab82178fd00cc1a6ad2c990ff2265d5
FOrhadul Islam <mdforhadul44@gmail.com>
2025-04-28 11:06:07 +0600

    add alll

--- backendapi/categorie/models.py
+++ backendapi/categorie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    producats = models.ManyToManyField('product.Producat', related_name='producats', blank=True)
+    categorie_image = models.ImageField(upload_to='categories/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
--- backendapi/product/models.py
+++ backendapi/product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Producat(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    
+    title1 = models.CharField(max_length=100, blank=True, null=True)
+    description1 = models.TextField(blank=True, null=True)
+    title2 = models.CharField(max_length=100, blank=True, null=True)
+    description2 = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    categories = models.ForeignKey('categorie.Categorie', on_delete=models.CASCADE, related_name='categories', blank=True, null=True)
+    image = models.ManyToManyField('ProducatImage', blank=True, related_name='producat_images')
+    
+    
+
+    def __str__(self):
+        return self.name
+
+
+class ProducatImage(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='producats/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name} - producat image"
