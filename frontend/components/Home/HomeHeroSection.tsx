ba2138dbeab2283e19a73a1380155e8c3bea441e
FOrhadul Islam <mdforhadul44@gmail.com>
2025-07-05 20:29:09 +0600

    addd alllll

--- frontend/components/Home/HomeHeroSection.tsx
+++ frontend/components/Home/HomeHeroSection.tsx
@@ -27,6 +27,55 @@ const SliderData = [
     title: "Slide 3",
     description: "This is the third slide description.",
   },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 7,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 8,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 9,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
+  {
+    id: 10,
+    image:
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    title: "Slide 3",
+    description: "This is the third slide description.",
+  },
 ];
 
 const HomeHeroSection = () => {
@@ -39,7 +88,7 @@ const HomeHeroSection = () => {
 
   return (
     <div className="container mx-auto h-auto">
-      <div className="flex gap-4 p-4">
+      <div className="flex gap-5 p-4 pt-5">
         <div className="w-full h-auto md:w-2/3">
           <EmblaCarousel
             slides={SliderData}
@@ -58,7 +107,7 @@ const HomeHeroSection = () => {
             orientation="vertical"
             className="w-full"
           >
-            <CarouselContent className="-mt-1 md:h-[350px] lg:h-[380px] xl:h-[410px]">
+            <CarouselContent className="-mt-1 md:h-[350px] lg:h-[375px] xl:h-[405px]">
                 {
                     SliderData.map((slide,index) => {
                         return (
@@ -68,7 +117,7 @@ const HomeHeroSection = () => {
                       <img
                         src= {slide.image}
                         alt={`Slide ${index + 1}`}
-                        className="w-full h-[346px] lg:h-[376px] xl:h-[406px] object-cover rounded-lg"
+                        className="w-full h-[345px] lg:h-[365px] xl:h-[400px] object-cover rounded-lg"
                       />
                     </CardContent>
                   </Card>
